d61ca2351dce0b8f79583e4ec85e5b242900d3db
guiferrao <ferraogui@hotmail.com>
2025-11-18 23:31:37 -0300

    Curso de Entity Framework separado em Modulos

---
+++ Modulo2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var context = new BlogDataContext())
+            {
+                // var tag = new Tag { Name = ".NET", Slug = "dotnet"};
+                // context.Tags.Add(tag);
+                // context.SaveChanges();
+
+                // var tag2 = new Tag { Name = "ASP.NET", Slug = "aspnet"};
+                // context.Tags.Add(tag2);
+                // context.SaveChanges();
+
+                //var tag = context.Tags.FirstOrDefault(x=>x.Id == 1);
+                //tag.Name = ".NET";
+                //tag.Slug = "dotnet";
+
+                //context.Update(tag);
+                //context.SaveChanges();
+
+                //var tag = context.Tags.FirstOrDefault(x=>x.Id == 1);
+
+                //context.Remove(tag);
+                //context.SaveChanges();
+
+                // var tags = context
+                //     .Tags
+                //     .AsNoTracking()
+                //     .ToList();
+
+                // foreach (var tag in tags)
+                // {
+                //     Console.WriteLine(tag.Id);
+                // }
+
+                // var tag = context
+                //     .Tags
+                //     .AsNoTracking()
+                //     .FirstOrDefault(x => x.Id == 3);
+                // Console.WriteLine(tag?.Name);
+            }
+        }
+    }
+}
